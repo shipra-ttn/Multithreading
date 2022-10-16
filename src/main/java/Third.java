9dcda874a902bf1998b79c4dfe8a97b64b6f14a1
Shipra Sharma <shipra sharma>
2022-10-17 00:21:04 +0530

    Initial commit

---
+++ src/main/java/Third.java
@@ -0,0 +1,33 @@
+public class Third {
+    private Integer counter = 0;
+    private void increment() {
+        this.counter++;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Third SynchronizedMethod = new Third();
+        Object lock = new Object();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                synchronized(lock) {
+                    SynchronizedMethod.increment();
+                }
+            }
+        }, "IncrementThread");
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                synchronized(lock) {
+                    SynchronizedMethod.increment();
+                }
+            }
+        }, "IncrementThread");
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        Thread.sleep(1000);
+        System.out.println(SynchronizedMethod.counter);
+    }
+}
+
